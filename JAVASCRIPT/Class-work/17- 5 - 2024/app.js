bcec5ada7964089402c314379bb0059e8f632047
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-19 17:57:27 +0500

    updated

---
+++ JAVASCRIPT/Class-work/17- 5 - 2024/app.js
@@ -0,0 +1,12 @@
+// event is click and onclick="" is attribute
+// 
+// onmouseover
+// onmouseout
+// this
+
+function greet(msg) {
+    console.log(msg);
+}
+function link(name) {
+    location = name
+}
