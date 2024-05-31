b0dd14feac4e9951b4bc373e68db6e0cb9cb381a
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-31 16:00:20 +0500

    ok

--- JAVASCRIPT/Class-work/31 - 5 - 2024/app.js
+++ JAVASCRIPT/Class-work/31 - 5 - 2024/app.js
@@ -5,39 +5,31 @@ let todoInputContainer = document.querySelectorAll(
   "#add-new-todo-container"
 )[0];
 let editInputEl = document.querySelectorAll(".edit-todo")[0];
-
 function add() {
   let todoVal = todoInput.value;
   printAllTodos(todoVal);
   todoInput.value = "";
 }
-
 function printAllTodos(newTodoText) {
   let newTodoList = makeTodoListEl(newTodoText);
   todoListDiv.appendChild(newTodoList);
 }
-
 function makeTodoListEl(newTodoText) {
   let pEl = document.createElement("P");
   let textNode = document.createTextNode(newTodoText);
   pEl.appendChild(textNode);
-
   let delBtn = document.createElement("BUTTON");
   let delText = document.createTextNode("Delete");
   delBtn.appendChild(delText);
   delBtn.setAttribute("onclick", "deleteTodo(this)");
-
   let editBtn = document.createElement("BUTTON");
   let editText = document.createTextNode("Edit");
   editBtn.appendChild(editText);
   editBtn.setAttribute("onclick", "editTodo(this)");
-
   pEl.appendChild(delBtn);
   pEl.appendChild(editBtn);
-
   return pEl;
 }
-
 function deleteTodo(el) {
   let targetNode = el.parentNode;
   let parentEl = el.parentNode.parentNode;
@@ -50,7 +42,6 @@ function editTodo(el) {
   targetText = el.parentNode.childNodes[0];
   editInputEl.value = targetText.nodeValue;
 }
-
 function upadateTodo() {
   targetText.nodeValue = editInputEl.value;
   editTodoContainer.className += " hide";
