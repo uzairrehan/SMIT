99cb13481bf8155716f18407a6c8a8fb5b7ce67b
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-07-13 12:55:04 +0500

    practiced

---
+++ REACT/12-7-2024/src/app/hello.tsx
@@ -0,0 +1,7 @@
+export default  function Create ({greet}){
+    return (
+        <div>
+            <h1>{greet}</h1>
+        </div>
+    );
+}
---
+++ REACT/12-7-2024/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Create from "./hello";
+
+export default function Home() {
+  return (
+      <Create
+       greet = "Hello jii"
+      />
+
+
+  );
+}
